adf75770c981e82be95596ee0fcb12c5f4ba09c9
mvanbesien <mvaawl@gmail.com>
2017-03-05 12:52:57 +0100

    Added Asimov World Seasons Date

---
+++ js/asimov.js
@@ -0,0 +1,24 @@
+var asimovCalendarValues = function(date) {
+	
+	var base = new Date(date.getFullYear(), 11, 21, 0, 0, 0, 0);
+	if (date.getTime() < base.getTime()) {
+		base.setFullYear(base.getFullYear() - 1);
+	}
+	var isLeap = isLeapYear(date.getFullYear());
+	
+	var daysCount = dayCounter(base, date);
+	
+	var wscLimits = [91, isLeap ? 92 : 91, 91];
+	var wscIndex = 0;
+	for (var i = 0; i < wscLimits.length && daysCount > wscLimits[i]; i++) {
+		wscIndex++;
+		daysCount -= wscLimits[i];
+	}
+	return [wscIndex, daysCount];
+}
+
+var asimovCalendarText = function(date) {
+	var dow = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	var values = asimovCalendarValues(date);
+	return (values[1] < 92 ? (dow[values[1] % 7] + ' '): '') + ['A', 'B', 'C', 'D'][values[0]]+ '-' + (values[1] < 10 ? "0" : "") + values[1];
+}
